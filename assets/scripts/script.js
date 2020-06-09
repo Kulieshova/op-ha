9fe71b0f0e8c57fba3ac29f4e706416cf2b41aff
Natalie <kulieshovan4@gmail.com>
2020-06-09 23:30:03 +0300

    Explore page

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -96,3 +96,76 @@ function myFunction() {
         .resize(resize)
         .trigger('resize');
   })(jQuery);
+
+
+
+  
+
+  /* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+
+for (i = 0; i < dropdown.length; i++) {
+  dropdown[i].addEventListener("click", function() {
+  this.classList.toggle("active");
+  var dropdownContent = this.nextElementSibling;
+  if (dropdownContent.style.display === "block") {
+  dropdownContent.style.display = "none";
+  } else {
+  dropdownContent.style.display = "block";
+  }
+  });
+}
+
+const d = 40;
+
+document.querySelectorAll('.rocket-button').forEach(elem => {
+
+    elem.querySelectorAll('.default, .success > div').forEach(text => {
+        charming(text);
+        text.querySelectorAll('span').forEach((span, i) => {
+            span.innerHTML = span.textContent == ' ' ? '&nbsp;' : span.textContent;
+            span.style.setProperty('--d', i * d + 'ms');
+            span.style.setProperty('--ds', text.querySelectorAll('span').length * d - d - i * d + 'ms');
+        });
+    });
+
+    elem.addEventListener('click', e => {
+        e.preventDefault();
+        if(elem.classList.contains('animated')) {
+            return;
+        }
+        elem.classList.add('animated');
+        elem.classList.toggle('live');
+        setTimeout(() => {
+            elem.classList.remove('animated');
+        }, 2400);
+    });
+
+});
+
+// Smooth scroll effect
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+});
